9dda5f5ce5d283908d6629cceec12fcdba7b237f
Allan Borges <allan5816@gmail.com>
2023-07-26 15:56:31 -0300

    adicionando novo projeto

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import style from "./Rodape.module.css";
+
+function Rodape(){
+    return(
+        <footer className={style.rodape}>
+            <h2>© 2023 Desenvolvido por Allan.</h2>
+        </footer>
+    )
+}
+
+export default Rodape;
